9db4f99aa1b30c32e8519a947b2a9d5ba1775f8b
tomasbarak <barakcinthia@gmail.com>
2021-12-29 15:05:18 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const axios = require('axios')
+const randomstring = require("randomstring");
+const parse = require('node-html-parser').parse;
+
+var attemtps = 0;
+function hacerGanarAPosta(){
+    let randid = randomstring.generate(40);
+    axios.post('https://api.premios.filo.news/api/users/update', {
+        candidates: [
+          "posta_sports",
+          "posta_sports",
+          "posta_sports",
+          "posta_sports",
+          "posta_sports",
+          "posta_sports",
+          "posta_sports",
+          "posta_sports",
+          "posta_sports",
+          "posta_sports"
+        ],
+        id: randid,
+}).then(res => {
+    console.log(`statusCode: ${res.status} attempt: ${attemtps} id: ${randid}`)
+    attemtps++;
+  }).catch(error => {
+    const root = parse(error.response.data);
+    console.log(root.querySelectorAll('pre').toString())
+})
+}
+hacerGanarAPosta()
+setInterval(function(){
+    hacerGanarAPosta()
+    hacerGanarAPosta()
+    hacerGanarAPosta()
+    hacerGanarAPosta()
+    hacerGanarAPosta()
+}, 100)
